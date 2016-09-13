f6215cc2ef329860e2f0c5129462eba826679f20
Ben Huson <ben@thewhiteroom.net>
2016-09-13 07:24:36 +0100

    Add `cite` tag support.

--- inline-quote-tag.php
+++ inline-quote-tag.php
@@ -81,6 +81,7 @@ class Q_Tag {
 	public function register_map_button( $buttons ) {
 
 		array_push( $buttons, 'separator', 'qtag' );
+		array_push( $buttons, 'separator', 'citetag' );
 
 		return $buttons;
 
@@ -97,6 +98,7 @@ class Q_Tag {
 	public function add_map_plugin( $plugin_array ) {
 
 		$plugin_array['qtag'] = plugins_url( 'js/tinymce/plugins/qtag/editor_plugin.js', __FILE__ );
+		$plugin_array['citetag'] = plugins_url( 'js/tinymce/plugins/citetag/editor_plugin.js', __FILE__ );
 
 		return $plugin_array;
 
---
+++ js/tinymce/plugins/citetag/editor_plugin.js
@@ -0,0 +1,47 @@
+/**
+ * $Id: editor_plugin.js 001 2009-03-25
+ *
+ * @author Ben Huson
+ * @copyright Copyright 2004-2008, Ben Huson, All rights reserved.
+ */
+
+(function() {
+	tinymce.create('tinymce.plugins.CiteTag', {
+		
+		// Init.
+		init : function(ed, url) {
+			var t = this;
+
+			t.editor = ed;
+
+			// Register commands
+			ed.addCommand('mceCiteTag', function() {
+				ed.execCommand('mceReplaceContent', false, ' <cite>{$selection}</cite> ');
+			});
+
+			// Register buttons
+			ed.addButton('citetag', {
+				title : 'Cite Tag',
+				cmd : 'mceCiteTag',
+				image : url + '/img/button.png'
+			});
+
+		},
+		
+		// Get Info
+		getInfo : function() {
+			return {
+				longname : 'Cite Tag',
+				author : 'Ben Huson',
+				authorurl : 'https://github.com/benhuson',
+				infourl : 'https://github.com/benhuson/inline-quote-tag',
+				version : tinymce.majorVersion + "." + tinymce.minorVersion
+			};
+		}
+
+	});
+
+	// Register plugin
+	tinymce.PluginManager.add('citetag', tinymce.plugins.CiteTag);
+	
+})();
